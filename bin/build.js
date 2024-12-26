c3c4d97138610a372e56af81561642dff75db37e
Quinten Clause <quintenclause@hotmail.com>
2024-12-27 00:44:27 +0100

    Add eleventy

--- bin/build.js
+++ bin/build.js
@@ -1,2 +1,15 @@
 #! /usr/bin/env node
 console.log("Building your webwolk-site");
+
+import Eleventy from "@11ty/eleventy";
+
+let elev = new Eleventy(
+    "./data/pages",
+    "./public",
+    {
+        config: function(eleventyConfig) {
+            eleventyConfig.setIncludesDirectory("../../node_modules/@quinten/webwolk-lib/_includes");
+        }
+    }
+);
+await elev.write();
--- bin/serve.js
+++ bin/serve.js
@@ -1,2 +1,17 @@
 #! /usr/bin/env node
 console.log("Serving your webwolk-site for development");
+
+import Eleventy from "@11ty/eleventy";
+
+let elev = new Eleventy(
+    "./data/pages",
+    "./public",
+    {
+        config: function(eleventyConfig) {
+            eleventyConfig.setIncludesDirectory("../../node_modules/@quinten/webwolk-lib/_includes");
+        }
+    }
+);
+await elev.init();
+await elev.watch();
+await elev.serve();
